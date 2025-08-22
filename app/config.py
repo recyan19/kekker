a6774cbab0d09df3d441ef68fdf8862dcb99170c
Yan Sokolovskii <yan.sokolovskii@progforce.com>
2025-08-22 12:40:22 +0200

    split .env files

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 """ Configuration settings for the FastAPI application."""
-
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -19,7 +19,10 @@ class Settings(BaseSettings):
     postgres_user: str
     postgres_db: str
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=f".env.{os.getenv('ENVIRONMENT', 'local')}",
+        env_file_encoding="utf-8"
+        )
 
 
 settings = Settings() # type: ignore
